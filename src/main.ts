184b0c00ee772c1b70001d10771aeaf7abbe0721
mikaellpc4 <mikaellpc4@gmail.com>
2023-02-26 19:27:43 -0300

    Port Env Heroku

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const serverPort = process.env.PORT || 3333;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3333);
+  await app.listen(serverPort);
 }
 bootstrap();
